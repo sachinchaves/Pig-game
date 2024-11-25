5993d274236d9a17c8ff31717c09faa51703d890
sachinchaves <sachin.chaves1991@gmail.com>
2024-11-25 08:43:33 -0800

    setting the game to 0

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+
+// Selecting Elements
+const score0Element = document.getElementById("score--0");
+const score1Element = document.getElementById("score--1");
+const diceElement = document.querySelector(".dice");
+
+// Setting the value to 0
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add("hidden");
